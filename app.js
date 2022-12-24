c3f514d5338a921be69d2e130b1ac08ad1c1fede
unknown <premrnjan@gmail.com>
2022-12-24 14:13:11 +0530

    Form is added to list new task

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+var taskItems = [];
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindOfDay: day, newTaskItems: taskItems });
+});
+
+app.post("/", function (req, res) {
+  var taskItem = req.body.taskName;
+  taskItems.push(taskItem);
+  res.redirect("/");
+
+});
+
+app.listen(3000, function () {
+  console.log("Server is up and running at port 3000");
+});
+
+
